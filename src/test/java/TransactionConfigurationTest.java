6f332d93b975ba24bb1ae17a88477d08623d4adb
Andrzej <andrzej.knapik1@gmail.com>
2018-03-19 11:44:51 +0100

    Modiefied tests

--- src/test/java/TransactionConfigurationTest.java
+++ src/test/java/TransactionConfigurationTest.java
@@ -3,20 +3,113 @@ import org.junit.Test;
 
 public class TransactionConfigurationTest {
     @Test
-    public void getInfos() {
+    public void getCustomerId() {
         //given
-        TransactionConfiguration uut = new TransactionConfiguration("1:20", "2018-03-08T00:00:00.000-0100:2018-03-08T23:59:59.999-0100",
-                "items.csv", "5:15", "1:30", "1000", "./output");
+        int[] customerIds = {1, 20};
+        int[] itemsCount = {5, 15};
+        int[] itemsQua = {1, 30};
+        TransactionConfiguration uut = new TransactionConfiguration(customerIds, "2018-03-08T00:00:00.000-0100:2018-03-08T23:59:59.999-0100",
+                "items.csv", itemsCount, itemsQua, 1000, ".");
 
         //when
 
         //then
-        Assert.assertEquals(uut.getRangeOfCustomerId(), "1:20");
-        Assert.assertEquals(uut.getRangeOfDate(), "2018-03-08T00:00:00.000-0100:2018-03-08T23:59:59.999-0100");
+        Assert.assertEquals(uut.getRangeOfCustomerId(), customerIds);
+    }
+
+    @Test
+    public void getDate() {
+        //given
+        int[] customerIds = {1, 20};
+        int[] itemsCount = {5, 15};
+        int[] itemsQua = {1, 30};
+        String date = "2018-03-08T00:00:00.000-0100:2018-03-08T23:59:59.999-0100";
+        TransactionConfiguration uut = new TransactionConfiguration(customerIds, date,"items.csv", itemsCount,
+                itemsQua, 1000, ".");
+
+        //when
+
+        //then
+        Assert.assertEquals(uut.getRangeOfDate(), date);
+    }
+
+    @Test
+    public void getFile() {
+        //given
+        int[] customerIds = {1, 20};
+        int[] itemsCount = {5, 15};
+        int[] itemsQua = {1, 30};
+        String date = "2018-03-08T00:00:00.000-0100:2018-03-08T23:59:59.999-0100";
+        TransactionConfiguration uut = new TransactionConfiguration(customerIds, date,"items.csv", itemsCount,
+                itemsQua, 1000, ".");
+
+        //when
+
+        //then
         Assert.assertEquals(uut.getFileWithItem(), "items.csv");
-        Assert.assertEquals(uut.getRangeOfQuantities(), "1:30");
-        Assert.assertEquals(uut.getRangeOfnumberOfItems(), "5:15");
-        Assert.assertEquals(uut.getNumberOfTrans(), "1000");
-        Assert.assertEquals(uut.getOurDir(), "./output");
+    }
+
+    @Test
+    public void getNumberOfItems() {
+        //given
+        int[] customerIds = {1, 20};
+        int[] itemsCount = {5, 15};
+        int[] itemsQua = {1, 30};
+        String date = "2018-03-08T00:00:00.000-0100:2018-03-08T23:59:59.999-0100";
+        TransactionConfiguration uut = new TransactionConfiguration(customerIds, date,"items.csv", itemsCount,
+                itemsQua, 1000, ".");
+
+        //when
+
+        //then
+        Assert.assertEquals(uut.getRangeOfnumberOfItems(), itemsCount);
+    }
+
+    @Test
+    public void getRangeOfQuantities() {
+        //given
+        int[] customerIds = {1, 20};
+        int[] itemsCount = {5, 15};
+        int[] itemsQua = {1, 30};
+        String date = "2018-03-08T00:00:00.000-0100:2018-03-08T23:59:59.999-0100";
+        TransactionConfiguration uut = new TransactionConfiguration(customerIds, date,"items.csv", itemsCount,
+                itemsQua, 1000, ".");
+
+        //when
+
+        //then
+        Assert.assertEquals(uut.getRangeOfQuantities(), itemsQua);
+    }
+
+    @Test
+    public void getNumberOfTrans() {
+        //given
+        int[] customerIds = {1, 20};
+        int[] itemsCount = {5, 15};
+        int[] itemsQua = {1, 30};
+        String date = "2018-03-08T00:00:00.000-0100:2018-03-08T23:59:59.999-0100";
+        TransactionConfiguration uut = new TransactionConfiguration(customerIds, date,"items.csv", itemsCount,
+                itemsQua, 1000, ".");
+
+        //when
+
+        //then
+        Assert.assertEquals(uut.getNumberOfTrans(), 1000);
+    }
+
+    @Test
+    public void getOutDir() {
+        //given
+        int[] customerIds = {1, 20};
+        int[] itemsCount = {5, 15};
+        int[] itemsQua = {1, 30};
+        String date = "2018-03-08T00:00:00.000-0100:2018-03-08T23:59:59.999-0100";
+        TransactionConfiguration uut = new TransactionConfiguration(customerIds, date,"items.csv", itemsCount,
+                itemsQua, 1000, ".");
+
+        //when
+
+        //then
+        Assert.assertEquals(uut.getOurDir(), ".");
     }
 }
