266f990f56a60e780bff0962e4dc4d26d5ce5835
Andrzej <andrzej.knapik1@gmail.com>
2018-03-21 10:28:23 +0100

    Add logger

--- src/main/java/CSVReader.java
+++ src/main/java/CSVReader.java
@@ -1,14 +1,19 @@
 import com.sun.org.apache.xpath.internal.SourceTree;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 
 public class CSVReader {
-    public Item[] readItemsFromFile(String path) throws IOException {
+    private final Logger CSVReaderLogger = LogManager.getLogger(CSVReader.class);
+
+    public Item[] readItemsFromFile(String path) {
         String line = "";
         List<Item> itemsList = new ArrayList<>();
         try (BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(path), "UTF8"))) {
+            CSVReaderLogger.info("Start reading from file " + path);
             br.readLine();
             while ((line = br.readLine()) != null) {
                 String[] itemPara = line.split(",");
@@ -17,7 +22,18 @@ public class CSVReader {
             }
             Item[] itemsArray = new Item[itemsList.size()];
             itemsList.toArray(itemsArray);
+            CSVReaderLogger.info("Items have been read");
             return itemsArray;
+        } catch (UnsupportedEncodingException e) {
+            CSVReaderLogger.info("Reading items is not possible " + e.getClass());
+            e.printStackTrace();
+        } catch (FileNotFoundException e) {
+            CSVReaderLogger.info("Reading items is not possible " + e.getClass());
+            e.printStackTrace();
+        } catch (IOException e) {
+            CSVReaderLogger.info("Reading items is not possible " + e.getClass());
+            e.printStackTrace();
         }
+        return null;
     }
 }
