e29c6c973543d8314cb44f0fd42ca3d08540d98c
Andrzej <andrzej.knapik1@gmail.com>
2018-04-02 16:30:55 +0200

    Add XMLWriter

--- src/main/java/writers/XMLWriter.java
+++ src/main/java/writers/XMLWriter.java
@@ -1,13 +1,46 @@
 package writers;
 
+import com.thoughtworks.xstream.XStream;
+import com.thoughtworks.xstream.io.xml.DomDriver;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import structures.Transaction;
-
-import java.io.BufferedWriter;
+import java.beans.XMLEncoder;
+import java.io.*;
 import java.util.List;
 
 public class XMLWriter implements IWriter {
+    private final Logger XMLWriterLogger = LogManager.getLogger(XMLWriter.class);
+    private final String dir;
+
+    public XMLWriter(String dir) {
+        this.dir = dir;
+    }
+
     @Override
     public boolean write(List<Transaction> transactionList, BufferedWriter writerBuff) {
-        return false;
+        String path ="";
+        BufferedWriter writer = null;
+        XMLEncoder encoder = null;
+        XMLWriterLogger.info("Start writing XMLs to files");
+        try {
+            for (Transaction tran : transactionList) {
+                path = dir + "/transaction" + tran.getId() + ".xml";
+                XStream mapping = new XStream(new DomDriver());
+                String xml = mapping.toXML(tran);
+                XMLWriterLogger.debug("Start writing transaction " + tran.getId());
+                if (writerBuff == null) {
+                    writer = new BufferedWriter(new FileWriter(path));
+                } else {
+                    writer = writerBuff;
+                }
+                writer.write(xml);
+                XMLWriterLogger.debug("Transaction " + tran.getId() + " successfully written");
+                writer.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return true;
     }
 }
