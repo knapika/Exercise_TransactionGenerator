273cea210a92bcd6ca788a0a9597abe290fb417a
Andrzej <andrzej.knapik1@gmail.com>
2018-05-14 19:14:51 +0200

    Fix problem with date get from properties file

--- src/main/java/readers/PropertiesReader.java
+++ src/main/java/readers/PropertiesReader.java
@@ -1,8 +1,8 @@
 package readers;
 
-import structures.Range;
-import structures.TransactionConfiguration;
-import writers.IWriter;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import structures.InputValues;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -11,29 +11,63 @@ import java.io.InputStream;
 import java.util.Properties;
 
 public class PropertiesReader {
-    public TransactionConfiguration getTransactionGenerationFromProperties() {
-        Range<Integer> rangeOfCustomerId = new Range<>(1, 20);
+    private final Logger PropertiesReaderLogger = LogManager.getLogger(PropertiesReader.class);
+    public InputValues getInputValues() {
+        String rangeOfCustomerId = "";
         String rangeOfDate ="";
         String fileWithItem = "";
-        Range<Integer> rangeOfnumberOfItems =  new Range<>(1, 5);
-        Range<Integer> rangeOfQuantities = new Range<>(1, 5);
-        int numberOfTrans = 100;
-        String outDir = ".";
-        String format = "JSON";
-        IWriter writer = null;
+        String rangeOfnumberOfItems = "";
+        String rangeOfQuantities = "";
+        String numberOfTrans = "";
+        String outDir = "";
+        String format = "";
 
         Properties prop = new Properties();
         InputStream input = null;
 
         try {
-            input = new FileInputStream("generator.properties");
-
+            input = new FileInputStream("src\\main\\resources\\generator.properties");
             prop.load(input);
+
+            if(prop.getProperty("itemsFile") != null) {
+                PropertiesReaderLogger.debug("Get itemsFile");
+                fileWithItem = prop.getProperty("itemsFile");
+            }
+            if(prop.getProperty("customerIds") != null) {
+                PropertiesReaderLogger.debug("Get customerIds");
+                rangeOfCustomerId = prop.getProperty("customerIds");
+            }
+            if(prop.getProperty("dateRange") != null) {
+                PropertiesReaderLogger.debug("Get dateRange");
+                rangeOfDate = prop.getProperty("dateRange");
+                rangeOfDate = rangeOfDate.replace("\"", "");
+            }
+            if(prop.getProperty("itemsCount") != null) {
+                PropertiesReaderLogger.debug("Get itemsCount");
+                rangeOfnumberOfItems = prop.getProperty("itemsCount");
+            }
+            if(prop.getProperty("itemsQuantity") != null) {
+                PropertiesReaderLogger.debug("Get itemsQuantity");
+                rangeOfQuantities = prop.getProperty("itemsQuantity");
+            }
+            if(prop.getProperty("outDir") != null) {
+                PropertiesReaderLogger.debug("Get outDir");
+                outDir = prop.getProperty("outDir");
+            }
+            if(prop.getProperty("eventsCount") != null) {
+                PropertiesReaderLogger.debug("Get eventsCount");
+                numberOfTrans = prop.getProperty("eventsCount");
+            }
+            if(prop.getProperty("format") != null) {
+                PropertiesReaderLogger.debug("Get format");
+                format = prop.getProperty("format");
+            }
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
-        return null;
+        return new InputValues(rangeOfCustomerId, rangeOfDate, fileWithItem, rangeOfnumberOfItems, rangeOfQuantities,
+                numberOfTrans, outDir, format);
     }
 }
